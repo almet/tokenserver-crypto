b94835b8ebf126c049c1068d272fce3d86224ce0
Alexis Metaireau <alexis@notmyidea.org>
2012-01-23 20:21:31 +0100

    yay. changes method signatures to make it work

--- crypto.cpp
+++ crypto.cpp
@@ -9,6 +9,7 @@ using namespace CryptoPP;
 using namespace std;
 
 #define DEFAULT_SALT_SIZE 128
+#define DEFAULT_KEY_SIZE 128
 
 namespace crypto{
 
@@ -18,7 +19,7 @@ namespace crypto{
      * @param int the number of bytes to generate
      * @returns byte* the generated salt, in bytes
      **/
-    byte* generateRandomSalt(int size = DEFAULT_SALT_SIZE){
+    byte* generateRandomSalt(int size=DEFAULT_SALT_SIZE){
 
         // Get a random number from the OS
         byte* random_salt;
@@ -47,19 +48,16 @@ namespace crypto{
      *              the key.
      * @returns byte* the derivated output keyring material
      **/
-    byte* deriveKey(byte* ikm, byte* salt, int size, byte* info = NULL){
+    byte* deriveKey(int size, byte* ikm, int ikm_size, byte* salt,
+                    int salt_size){
 
-        byte* okm;
-
-        // fill-in the result with zeroes
-        memset(okm, 0, sizeof(okm));
+        byte* okm = new byte[size];
 
         HMACKeyDerivationFunction<SHA256> hkdf;
         hkdf.DeriveKey(okm, size,
-                       ikm, sizeof(ikm),
-                       salt, sizeof(salt),
-                       info, sizeof(info));
-
+                       ikm, ikm_size,
+                       salt, salt_size,
+                       NULL, 0);
         return okm;
     }
 
@@ -83,7 +81,7 @@ namespace crypto{
         // obtain file size:
         fseek(pFile, 0, SEEK_END);
         lSize = ftell(pFile);
-        rewind (pFile);
+        rewind(pFile);
       
         // allocate memory to contain the whole file:
         buffer = (char*) malloc(sizeof(char) * lSize);
@@ -104,35 +102,36 @@ namespace crypto{
         return (byte*) buffer;
     }
 
-    void writeFile(string filename, byte* content){
+    void writeFile(string filename, byte* content, int size){
         ofstream oFile;
         oFile.open(filename.c_str(), ofstream::binary);
-        oFile.write((char*) content, sizeof(content));
+        oFile.write((char*) content, size);
         oFile.close();
     }
 }
 
 int main(int argc, const char *argv[])
 {
-    byte ikm[80] = { 
-        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
-        0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
-        0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21,
-        0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d,
-        0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
-        0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45,
-        0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f };
+    // byte ikm[80] = { 
+    //     0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09,
+    //     0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15,
+    //     0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f, 0x20, 0x21,
+    //     0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d,
+    //     0x2e, 0x2f, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39,
+    //     0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45,
+    //     0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f };
 
-    crypto::writeFile("master", ikm);
+    byte* salt = crypto::generateRandomSalt(DEFAULT_SALT_SIZE);
+    cout << salt << endl;
+
+    crypto::writeFile("master", salt, DEFAULT_SALT_SIZE);
 
     byte* master = crypto::readFile("master");
-    byte* salt = crypto::generateRandomSalt(78);
-    byte* okm = crypto::deriveKey(master, salt, 78);
-
-
     cout << master << endl;
-    cout << salt << endl;
-    cout << okm << endl;
 
+    byte* okm = crypto::deriveKey(DEFAULT_SALT_SIZE,
+                                  master, DEFAULT_SALT_SIZE,
+                                  salt, DEFAULT_SALT_SIZE);
+    cout << okm << endl;
     return 0;
 }
